5f4af4b9fc1e8986c90d5ec19159f0e424eafdd9
Hasan Ul Kabir Utsho <mcutsho96@gmail.com>
2025-12-08 12:19:30 +0600

    Banner Image Animation using React Motion

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,7 +5,7 @@ import Navbar from '../pages/Shared/Navbar';
 
 const RootLayout = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,41 +1,55 @@
 import React from "react";
 import { motion, scale } from "motion/react";
+import team1 from "../../assets/team/team1.jpg";
+import team2 from "../../assets/team/team2.jpg";
 
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-          <motion.h1
+        <div className="flex-1">
+          <motion.img
+            src={team1}
+            animate= {{ y: [80,100,80] }}
+            transition={{ duration: 5, repeat: Infinity }}
+            className="max-w-sm border-blue-500 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl"
+          />
+          <motion.img
+            src={team2}
+            animate= {{ x: [100,150,100] }}
+            transition={{ duration: 10, delay: 5, repeat: Infinity }}
+            className="max-w-sm border-blue-500 border-s-8 border-b-8 rounded-t-[40px] rounded-br-[40px] shadow-2xl"
+          />
+        </div>
+        <div className="flex-1">
+          {/* <motion.h1
             animate={{
-              rotate: 360,
-              scale: 1,
-              transition: { duration: 2 },
+              rotate: 180,
+              x: 200,
+              y: -200,
+              // scale: 1,
+              transition: { duration: 4 },
             }}
             className="text-5xl font-bold"
           >
             Latest Jobs For You!
+          </motion.h1> */}
+          <motion.h1
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 2 }}
+            className="text-5xl font-bold"
+          >
+            Remote{" "}
+            <motion.span
+              animate={{ color: ["#F54927", "#49F527", "#8727F5"] }}
+              transition={{ duration: 3, repeat: Infinity }}
+              style={{ display: "inline-block" }}
+            >
+              Jobs
+            </motion.span>{" "}
+            For You!
           </motion.h1>
-         <motion.h1
-  initial={{ scale: 0 }}
-  animate={{ scale: 1 }}
-  transition={{ duration: 2 }}
-  className="text-5xl font-bold"
->
-  Remote{" "}
-  <motion.span
-    animate={{ color: ["#F54927", "#49F527", "#8727F5"] }}
-    transition={{ duration: 3, repeat: Infinity }}
-    style={{ display: "inline-block" }}
-  >
-    Jobs
-  </motion.span>{" "}
-  For You!
-</motion.h1>
 
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
